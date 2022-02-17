cd24935f09891a57bafc33eca15b78a6316e0c55
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-02-17 16:15:01 +0300

    Assert minitest for user models that look up to validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(name: "Example User", email: "example@user");
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should not be valid" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
 end
