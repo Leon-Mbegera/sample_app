175d0ee285d659f6474143d69e1777dac516d65a
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-02-24 15:36:32 +0300

    Position and style user gravatar and name

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
 
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=monsterid"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
