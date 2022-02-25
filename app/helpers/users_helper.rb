bd5e58266fcc58cc2d1d6fe2e5ac64e2707d10fb
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-02-25 22:22:34 +0300

    Instantiate a new user object to use with the form builder for the user sign up page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,10 @@ module UsersHelper
 
   # Returns the gravatar for a given user
 
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 40 })
+    size = options[:size]
     gravatar_id = Digest::MD5::hexdigest(user.email)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=monsterid"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=monsterid&s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
