4f13491c8b068d50d5396241c3b7144da49f0661
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-03-14 15:25:08 +0300

    Correct test for an invalid sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,15 +1,18 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  get signup_path
 
-  assert_no_difference 'User.count' do
-    post users_path, params: { user: {
-      name:  "",
-      email: "user@invalid",
-      password:              "foo",
-      password_confirmation: "bar"
-    }}
+  test 'Test for an invalid user sign up' do
+    get signup_path
+  
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {
+        name:  "",
+        email: "user@invalid",
+        password:              "foo",
+        password_confirmation: "bar"
+      }}
+    end
+    assert_template 'users/new'
   end
-  assert_template 'users/new'
 end
