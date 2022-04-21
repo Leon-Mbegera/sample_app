2ebd348ed3df0d4828e86bff2e49953457ac848f
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-04-21 11:41:50 +0300

    Create routes to handle user login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'session/new'
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help'
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
   resources :users
 end
