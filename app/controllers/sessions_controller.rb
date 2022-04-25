ef764699ddd4fb800f45f7795e021510ee67436a
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-04-25 20:11:49 +0300

    Complete the user's login, session creation, and page redirection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid username or password, Please try again !"
       render 'new'
