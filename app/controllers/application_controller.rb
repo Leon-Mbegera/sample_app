84f5a8c2550bfbeb378fab410a550395fbd5cd87
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-04-26 15:28:54 +0300

    Transfer methods to SessionsHelper module and include it in the applicationcontroller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
-
   include SessionsHelper
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  # Retrieving the logged in user in every page they visit
-  def current_user
-    @current_user || = User.find_by(id: session[:user_id])
-  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Retrieving the logged in user in every page they visit
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
