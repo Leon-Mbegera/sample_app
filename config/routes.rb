e014aaa32e936aa0a1f878ec725fbf2fe256c076
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-04-29 16:26:02 +0300

    Correct route to log out a current user

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
   resources :users
 end
